d6b613afa1201fa4fbd378c017cdc73dbfc74494
xwipeoutx@gmail.com <xwipeoutx@gmail.com@cd643a9f-10e3-0e01-14f8-47b6bb6b4944>
2012-01-25 11:40:58 +0000

    Added https support

--- undirect.js
+++ undirect.js
@@ -1,21 +1,23 @@
 // Google
-var googRegex = /http.\/\/.+google[^\/]*/gi;
-if (document.location.href.match(googRegex)) {
-                 
-  var scrubLinks = function() {
-    var els = document.getElementsByTagName('a');
-    for(var i=0, il=els.length; i<il; i++) {
-      var el = els[i];
-      var ni = el.attributes.getNamedItem('onmousedown');
-      if (ni && ni.textContent.substring(0, 10) === "return rwt") {
-        el.attributes.removeNamedItem('onmousedown');
-      }    
-    }
-  };
+(function() {
+  var googRegex = /https?.\/\/.+google[^\/]*/gi;
+  if (document.location.href.match(googRegex)) {
+                   
+    var scrubLinks = function() {
+      var els = document.getElementsByTagName('a');
+      for(var i=0, il=els.length; i<il; i++) {
+        var el = els[i];
+        var ni = el.attributes.getNamedItem('onmousedown');
+        if (ni && ni.textContent.substring(0, 10) === "return rwt") {
+          el.attributes.removeNamedItem('onmousedown');
+        }    
+      }
+    };
   
-  var timeoutId = false;
-  document.body.addEventListener("DOMNodeInserted", function() {
-    if (timeoutId) clearTimeout(timeoutId)
-    timeoutId = setTimeout(scrubLinks, 1000);
-  }, false);
-};
+    var timeoutId = false;
+    document.body.addEventListener("DOMNodeInserted", function() {
+      if (timeoutId) clearTimeout(timeoutId)
+      timeoutId = setTimeout(scrubLinks, 1000);
+    }, false);
+  };
+})();
