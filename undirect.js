544aefe555181fa823804a25fa30f6e0dfa2515e
xwipeoutx <xwipeoutx@gmail.com>
2014-05-17 23:07:52 +1000

    Updated script to latest manifest, script now works again on the googles.

--- undirect.js
+++ undirect.js
@@ -1,23 +1,32 @@
-// Google
-(function() {
-  var googRegex = /https?.\/\/.+google[^\/]*/gi;
-  if (document.location.href.match(googRegex)) {
-                   
-    var scrubLinks = function() {
-      var els = document.getElementsByTagName('a');
-      for(var i=0, il=els.length; i<il; i++) {
-        var el = els[i];
-        var ni = el.attributes.getNamedItem('onmousedown');
-        if (ni && ni.textContent.substring(0, 10) === "return rwt") {
-          el.attributes.removeNamedItem('onmousedown');
-        }    
-      }
-    };
-  
-    var timeoutId = false;
-    document.body.addEventListener("DOMNodeInserted", function() {
-      if (timeoutId) clearTimeout(timeoutId)
-      timeoutId = setTimeout(scrubLinks, 1000);
-    }, false);
-  };
-})();
+var fn = (function() {
+    var googRegex = /https?.\/\/.+google[^\/]*/gi;
+    if (document.location.href.match(googRegex)) {
+
+        var expectedRwt = function() { return true; };
+
+        var scrubLinks = function() {
+            if (window.rwt && window.rwt != expectedRwt) {
+                delete window.rwt;
+                Object.defineProperty(window, 'rwt', {
+                    value: expectedRwt,
+                    writable: false
+                });
+            }
+        };
+
+        scrubLinks();
+        var timeoutId = 0;
+        document.body.addEventListener("DOMNodeInserted", function() {
+            if (timeoutId) clearTimeout(timeoutId)
+            timeoutId = setTimeout(scrubLinks, 1000);
+        }, false);
+    }
+});
+
+var fnContents = fn.toString();
+var executeFnScript = '(' + fnContents + ')();';
+
+var script = document.createElement('script');
+script.textContent = executeFnScript;
+(document.head||document.documentElement).appendChild(script);
+script.parentNode.removeChild(script);
