5bd4fbd9a46e5ddfb31e6a9a4c107d330133e1fc
xwipeoutx <xwipeoutx@gmail.com>
2014-05-17 23:23:54 +1000

    Fixed a bad if statement... script no longer returns right at the start

--- undirect.js
+++ undirect.js
@@ -1,38 +1,37 @@
 (function() {
     var googlePagesPattern = /https?.\/\/.+google[^\/]*/gi;
-    if (!document.location.href.match(googlePagesPattern)) {
+    if (!document.location.href.match(googlePagesPattern))
         return;
 
-        var scriptToExecute = (function() {
-            var expectedRwt = function() { return true; };
+    var scriptToExecute = (function() {
+        var expectedRwt = function() { return true; };
 
-            var replaceRwtFunction = function() {
-                if (window.rwt && window.rwt != expectedRwt) {
-                    delete window.rwt;
-                    Object.defineProperty(window, 'rwt', {
-                        value: expectedRwt,
-                        writable: false
-                    });
-                }
-            };
+        var replaceRwtFunction = function() {
+            if (window.rwt && window.rwt != expectedRwt) {
+                delete window.rwt;
+                Object.defineProperty(window, 'rwt', {
+                    value: expectedRwt,
+                    writable: false
+                });
+            }
+        };
 
-            replaceRwtFunction();
+        replaceRwtFunction();
 
-            var timeoutId = 0;
-            document.body.addEventListener("DOMNodeInserted", function() {
-                if (timeoutId) clearTimeout(timeoutId)
-                timeoutId = setTimeout(replaceRwtFunction, 1000);
-            }, false);
-        });
+        var timeoutId = 0;
+        document.body.addEventListener("DOMNodeInserted", function() {
+            if (timeoutId) clearTimeout(timeoutId)
+            timeoutId = setTimeout(replaceRwtFunction, 1000);
+        }, false);
+    });
 
-        // Write script to page - since the plugin works in an isolated world, this gives us the
-        // ability to replace javascript added by the page
-        var fnContents = scriptToExecute.toString();
-        var executeFnScript = '(' + fnContents + ')();';
+    // Write script to page - since the plugin works in an isolated world, this gives us the
+    // ability to replace javascript added by the page
+    var fnContents = scriptToExecute.toString();
+    var executeFnScript = '(' + fnContents + ')();';
 
-        var script = document.createElement('script');
-        script.textContent = executeFnScript;
-        (document.head || document.documentElement).appendChild(script);
-        script.parentNode.removeChild(script);
-    }
+    var script = document.createElement('script');
+    script.textContent = executeFnScript;
+    (document.head || document.documentElement).appendChild(script);
+    script.parentNode.removeChild(script);
 })();
